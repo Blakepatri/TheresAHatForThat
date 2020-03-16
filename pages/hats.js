5f9581eb5ba2f9f207dd8c353b75038542f7036a
Blake Patriquin <bl978289@dal.ca>
2020-03-16 15:44:02 -0300

    added hats html and css

---
+++ pages/hats.js
@@ -0,0 +1,61 @@
+function render() {
+	return (
+		`<div class="hats">
+			<div class="row_hats">
+  				<div class="column_hats">
+  					<img src="..\images\hat1.jpg" class="img_hats">
+  					<h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>
+  				</div>
+  				<div class="column_hats">
+  					<img src="..\images\hat2.jpg" class="img_hats">
+            <h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>					
+  				</div>
+  				<div class="column_hats">
+  					<img src="..\images\hat3.jpg" class="img_hats">
+            <h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>					
+  				</div>
+          <div class="column_hats">
+            <img src="..\images\hat3.jpg" class="img_hats">
+            <h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>         
+          </div>
+          <div class="column_hats">
+            <img src="..\images\hat3.jpg" class="img_hats">
+            <h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>         
+          </div>
+          <div class="column_hats">
+            <img src="..\images\hat3.jpg" class="img_hats">
+            <h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>         
+          </div>
+          <div class="column_hats">
+            <img src="..\images\hat3.jpg" class="img_hats">
+            <h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>         
+          </div>
+          <div class="column_hats">
+            <img src="..\images\hat3.jpg" class="img_hats">
+            <h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>         
+          </div>
+          <div class="column_hats">
+            <img src="..\images\hat3.jpg" class="img_hats">
+            <h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>         
+          </div>
+          <div class="column_hats">
+            <img src="..\images\hat3.jpg" class="img_hats">
+            <h1 class="h1_hats">Hat Of The Day</h1>
+            <h1 class="h1_hats">Hat Of The Day</h1>         
+          </div>                                                                      
+			</div>
+		</div>`
+	);
+}
+
+
+module.exports.render = render;
