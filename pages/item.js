e59fa28a457a8a74b6f0ecd3455f9c78986e31bf
Blake Patriquin <bl978289@dal.ca>
2020-03-17 14:38:56 -0300

    added item html and css

---
+++ pages/item.js
@@ -0,0 +1,13 @@
+function render() {
+	return (
+		`<div class="item">
+      		<img src="..\images\hat1.jpg">
+      		<h1>Price</h1>
+      		<h1>Add to Cart</h1>
+      		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in blandit nisl. In hac habitasse platea dictumst. Etiam id tellus at sapien tristique euismod. Donec ante enim, commodo id venenatis eleifend, molestie vitae ex. Proin efficitur nibh eu neque porttitor, pretium pharetra nunc molestie. In semper, mi vel euismod tempor, ipsum tellus lobortis nisl, et blandit mauris dolor vel ligula. Nulla ut molestie felis. Fusce quis arcu condimentum, sagittis dui eu, commodo leo.</p>
+		</div>`
+	);
+}
+
+
+module.exports.render = render;
