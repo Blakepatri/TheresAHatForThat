977d8b0f27ab4137c011b2ab6c759650a0a4f9a7
Blake Patriquin <bl978289@dal.ca>
2020-03-29 15:36:30 -0300

    altered and added links to the nav bar.

--- elements/nav.js
+++ elements/nav.js
@@ -3,17 +3,16 @@ Renderer for the navigation bar
 **/
 function render() {
 return (`
-<div class="nav-bar">
-	<a class="active" href="#home">Home</a>
-  	<div class="nav-bar-right">
-    	<a href="#hats">Hats</a>
-    	<a href="#account">Account</a>
-    	<a href="#order">Orders</a>
-    	<a href="#about">Abou</a>
-    	<a href="#log-out">Log Out</a>
-    	<a href="#search">Search</a>
-  	</div>
-</div>
+		<div class="nav-bar">
+			<a class="active" href="/home">Home</a>
+  			<div class="nav-bar-right">
+    			<a href="/hats">Hats</a>
+    			<a href="/account">Account</a>
+    			<a href="/orders">Orders</a>
+          		<a href="/cart">Cart</a>
+    			<a href="/logout">Log Out</a>
+  			</div>
+		</div>
 `);
 }
 
